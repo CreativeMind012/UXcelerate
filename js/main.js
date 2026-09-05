fd3954004d3abc47b3d3a5e539230b5bc5314e10
CreativeMind012 <88550031+CreativeMind012@users.noreply.github.com>
2026-09-05 13:48:19 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+function loadData() {
+  const response = fetch('FakeData.json');
+  const data = response.json();
+  console.log(data.robots);
+}
+loadData();
 
+// do:loses signal ,found a survivor, route blocked , hazard found, last updated
